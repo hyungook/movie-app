ccc0dd6a36f5148400d244bed44054b16b347771
오형욱 <nenonenoberneno@gmail.com>
2021-01-27 21:35:19 +0900

    Reusable Components with JSX + props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({favourite}) {
+  console.log(favourite);
+  return <h3>I Like {favourite}</h3>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello!!</h1>
+      <Food favourite="kimchi" />
+      <Food favourite="ramen" />
+      <Food favourite="samgiopsal" />
+      <Food favourite="chikumi" />
     </div>
   );
 }
