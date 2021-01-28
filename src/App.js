ec280b80bf5da87de0f68de9aa890a6db1a8ae12
오형욱 <nenonenoberneno@gmail.com>
2021-01-28 23:49:48 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -2,20 +2,39 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello constructor")
+  }
   state = {
     count: 0
   }
 
 add =() => {
-  console.log("add");
+  // console.log("add");
   this.setState(current => ({count: current.count + 1}));
 }
 minus = () => {
-  console.log("minus");
+  // console.log("minus");
   this.setState(current => ({count: current.count - 1}));
 }
 
+componentDidMount() {
+  console.log("componentDidMount");
+};
+
+componentWillUnmount() {
+  console.log("componentWillUnmount");
+}
+
+componentDidUpdate() {
+  console.log("componentDidUPdate");
+}
+
   render() {
+
+    console.log("render");
+
     return<div>
         <h1>the number is : {this.state.count}</h1>
         <button onClick={this.add}> + </button>
